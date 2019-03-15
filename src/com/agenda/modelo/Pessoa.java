b5f25627a14e49544419b5886076caf3fff99dee
LABORATORIO_INFO <LABORATORIO_INFO@10.67.96.52>
2019-03-14 22:25:41 -0300

    Melhorei o formulario e criei uma classe

---
+++ src/com/agenda/modelo/Pessoa.java
@@ -0,0 +1,35 @@
+package com.agenda.modelo;
+
+public class Pessoa {
+private String nome;
+private String phone;
+private String email;
+private String end;
+
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getEnd() {
+	return end;
+}
+public void setEnd(String end) {
+	this.end = end;
+}
+
+
+}
--- src/com/agenda/servlet/CadastroUsuarioServlet.java
+++ src/com/agenda/servlet/CadastroUsuarioServlet.java
@@ -7,12 +7,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.agenda.modelo.Pessoa;
+
 public class CadastroUsuarioServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		request.getParameter("name");
+		request.getParameter("phone");
+		request.getParameter("mail");
+		request.getParameter("end");
+		
+		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome(request.getParameter("name"));
+		pessoa.setPhone(request.getParameter("phone"));
+		pessoa.setEmail(request.getParameter("email"));
+		pessoa.setEnd(request.getParameter("end"));
+		
+		
 		System.out.println(request.getParameter("name"));
+		System.out.println(request.getParameter("phone"));
+		System.out.println(request.getParameter("mail"));
+		System.out.println(request.getParameter("end"));
 	}
 }

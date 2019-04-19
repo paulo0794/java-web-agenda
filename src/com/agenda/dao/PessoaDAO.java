a575b3cdf9fd6350d65a2b891f0a62bbb237ce75
Paulo Henrique <paulo.oliveira438@etec.sp.gov.br>
2019-04-18 21:57:19 -0300

    Criei novos metodos na classe PessoaDAO e na classe CadastroUsuarioServlet.

--- src/com/agenda/dao/PessoaDAO.java
+++ src/com/agenda/dao/PessoaDAO.java
@@ -1,11 +1,15 @@
 package com.agenda.dao;
 
 import java.sql.SQLException;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import com.agenda.modelo.Pessoa;
 import com.agenda.util.ConnectionFactory;
 import com.mysql.jdbc.Connection;
+
 public class PessoaDAO {
 	
 	private Connection connection;
@@ -28,9 +32,48 @@ public class PessoaDAO {
 		stmt.execute();
 		stmt.close();
 		
+		buscaPessoas();
 	}catch (SQLException e) {
 		throw new RuntimeException(e);
 	}
+	}
+	
+	public List<Pessoa> buscaPessoas(){
+		
+		String SQL = "select * from pessoas";
+		
+		try {
+			
+			this.connection = new ConnectionFactory().getConnetion();
+			PreparedStatement stmt = this.connection.prepareStatement(SQL);
+			
+			List<Pessoa> pessoas = new ArrayList<Pessoa>();
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				Pessoa pessoa = new Pessoa();
+				
+				pessoa.setNome(rs.getString("nome"));
+				pessoa.setEmail(rs.getString("email"));
+				pessoa.setEnd(rs.getString("endereco"));
+				pessoa.setPhone(rs.getString("telefone"));
+				
+				
+				pessoas.add(pessoa);
+				
+			}
+			
+			stmt.close();
+			this.connection.close();
+			System.out.println(pessoas);
+			return pessoas;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+		
+		
 	}
 
 }
--- src/com/agenda/service/CadastroUsuarioService.java
+++ src/com/agenda/service/CadastroUsuarioService.java
@@ -1,5 +1,7 @@
 package com.agenda.service;
 
+import java.util.List;
+
 import com.agenda.dao.PessoaDAO;
 import com.agenda.modelo.Pessoa;
 
@@ -10,8 +12,16 @@ public class CadastroUsuarioService {
 		PessoaDAO dao = new PessoaDAO();
 		
 		dao.recebe(pessoa);
+		
+		System.out.println(buscaPessoas());
 
 	}
+	
+	public List<Pessoa> buscaPessoas() {
+		
+		PessoaDAO dao = new PessoaDAO();
+		return dao.buscaPessoas();
+	}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 	
 
 }
